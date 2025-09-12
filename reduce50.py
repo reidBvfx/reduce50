3e6430e855dfc4156e3a6d1ab1b9a6807247c0dd
reidvfx33 <152922289+reidvfx33@users.noreply.github.com>
2025-09-12 14:45:15 -0400

    works on hardEdge geo

--- reduce50.py
+++ reduce50.py
@@ -1,105 +1,108 @@
+# STYLE ***************************************************************************
+# content = evenly reduce polymesh by 50 %
+#           selects everyother edge loop
+#           requires selection of one vertical and one horizontal line
+# date    = 2025-09-12
+#
+# author  = Reid Bryan (reidwarhola@gmail.com)
+# **********************************************************************************
 import maya.cmds as cmds
 import re
 
-cmds.polySelect( 'pCube1', edgeRing=1 )
-orderedEdges = cmds.ls(orderedSelection=True, flatten=True)
-def correctNameFace(facesList):
-        """ removed range from bracket [x:y] and inserts value in range into new list"""
-        temp_faceList = []
-        for face in facesList:
-                objName = face.split('.f')[0]
-                if ":" in face:  
-                    nPCompile = re.compile(r'(\d+)') 
-                    numbers = nPCompile.findall(face)
-                    x = len(numbers)
-                    for i in range(int(numbers[x-2]), int(numbers[x-1])+1):
-                        temp_faceList.append(objName + ".f[" +str(i) + ']') 
-                        i += 1
-                else:
-                    temp_faceList.append(face)
-        return temp_faceList    
 
-def correctEdgeFace(facesList):
-        """ removed range from bracket [x:y] and inserts value in range into new list"""
-        temp_faceList = []
-        for face in facesList:
-                objName = face.split('.e')[0]
-                if ":" in face:  
-                    numbers = []
-                    nPCompile = re.compile(r'(\d+)') 
-                    numbers = nPCompile.findall(face)
-                    x = len(numbers)
-                    for i in range(int(numbers[x-2]), int(numbers[x-1])+1):
-                        temp_faceList.append(objName + ".e[" +str(i) + ']') 
-                        i += 1
-                else:
-                    temp_faceList.append(face)
-        return temp_faceList         
+def getName(item, type):
+    type = getType(type)
+    objName = item.split(type)[0]
+    return objName
 
-def correctNameVertex(vertexList):
-        """ removed range from bracket [x:y] and inserts value in range into new list"""
-        temp_vertexList = []
-        for vertex in vertexList:
-                objName = vertex.split('.vtx')[0]
-                # correct naming convention
-                if ":" in vertex:  
-                    nPCompile = re.compile(r'(\d+)') 
-                    numbers = nPCompile.findall(vertex)
-                    x = len(numbers)
-                    for i in range(int(numbers[x-2]), int(numbers[x-1])+1):
-                        temp_vertexList.append(objName + ".vtx[" +str(i) + ']') 
-                        i += 1
-                else:
-                    temp_vertexList.append(vertex)
-        return temp_vertexList        
+def getNumber(item, type):
+    """ args:   poly asset
+                string of type of asset
+        return: number inside brackets of object """
+    
+    match type:
+        case "edge":
+            type = '.e'
+        case "face":
+            type = '.f'
+    
+    bracket = item.split(type)[1]
+    nPattern = re.compile(r'(\d+)') 
+    number = nPattern.findall(bracket)[0]
+    return number
 
+def removeBrackets(set, type):
+    type = getType(type)
+    correctedNames = []
+    for item in set:
+        objName, bracket = item.split(type)
+        if ":" in bracket:
+            nPattern = re.compile(r'(\d+)') 
+            numbers = nPattern.findall(bracket)
+            for i in range(int(numbers[0]), int(numbers[1])+1):
+                correctedNames.append(objName + type + "[" + str(i) + "]")
+                i += 1
+        else:
+            correctedNames.append(item)
 
-testEdge = orderedEdges[0]
-def findEdgeLoop(testEdge):
-    testV = cmds.polyListComponentConversion(testEdge, tv = True, bo = True)
-    testV = correctNameVertex(testV)
-    testF = cmds.polyListComponentConversion(testEdge, tf = True, bo = True)
-    testF = correctNameFace(testF)
-    testE = [] 
-    for each in testF:
-        edge = cmds.polyListComponentConversion(each, te= True, bo = True)
-        for e in edge:
-            testE.append(e)
-    testE = correctEdgeFace(testE)   
-    nextEdge = []
-    for v in testV:
-        
-        rEdges = cmds.polyListComponentConversion(v, te = True, bo = True)
-        for e in rEdges:
-            
-            nextEdge.append(e)
-    nextEdge = correctEdgeFace(nextEdge)
-    next = ""
+    return correctedNames
+
+def getType(n):
+    match n:
+        case "edge":
+            type = '.e'
+        case "face":
+            type = '.f'
+        case "vertex":
+            type = '.vtx'
+    return type
+
+def findEdgeLoop(evenEdges):
+    """ find an edge loop by comparing all the edges of neighboring faces to all the edges of neighboring vertices
+        the difference is the edgeloop"""
+    
+    faces = cmds.polyListComponentConversion(evenEdges, tf = True, bo = True)
+    faces = removeBrackets(faces, "face")
+    fEdges = [] 
+    for face in faces:
+        fEdges.extend(cmds.polyListComponentConversion(face, te= True, bo = True))
+    fEdges = removeBrackets(fEdges, "edge")   
+
+    vertices = cmds.polyListComponentConversion(evenEdges, tv = True, bo = True)
+    vertices = removeBrackets(vertices, "vertex")
+    
+    vEdges = []
+    for v in vertices:
+        vEdges.extend(cmds.polyListComponentConversion(v, te = True, bo = True))
+    vEdges = removeBrackets(vEdges, "edge")
     edgeLoop = []
-    #edgeLoop.append(testEdge)
-    for each in nextEdge:
-        if each not in testE:
-            edgeLoop.append(each)
+    for edge in vEdges:
+        if edge not in fEdges:
+            edgeLoop.append(edge)
     return edgeLoop
 
-def test(index):
-    orderedEdges = cmds.polySelect( 'pCube1', edgeRing= index )
-    evenOE = []
-    for j in range(len(orderedEdges)):
+def getEdges(objName, index):
+    """ find every other edge in edgeRing and the find edge loop of each edge"""
+    edgeRing = cmds.polySelect( objName, edgeRing= index )
+    evenEdges = [] # get every other edge in ring
+    for j in range(len(edgeRing)):
          if j  % 2 != 0:
-            evenOE.append('pCube1.e[' + str(orderedEdges[j]) + "]")
+            evenEdges.append(objName + '.e[' + str(edgeRing[j]) + "]")
 
-    cmds.select(evenOE)
-    for each in evenOE:
+    cmds.select(evenEdges)
+    first = True
+    allEdges = []
+    for each in evenEdges:
         edgeLoop = [each]
-        for i in range(100):
+        edgeLoopLength = 100
+        for i in range(edgeLoopLength):
             try:
                 secondEdge =  findEdgeLoop(edgeLoop[i])
             except:
                 break
             
             if secondEdge == "":
-                i = 200
+                i = edgeLoopLength + 100 
             else:
                 newE = False
                 for each in secondEdge:
@@ -107,19 +110,33 @@ def test(index):
                         edgeLoop.append(each)
                         newE = True
                 if newE == False:
-                    i = 200           
+                    i = edgeLoopLength + 100           
                 else:
                     i += 1            
 
-        
-            cmds.select(edgeLoop, add = True)
-    selected = cmds.ls(sl=True,long=True)
-    return selected 
+        allEdges.extend(edgeLoop)
+    return allEdges 
+
+def start():
+    
+    selected = removeBrackets(cmds.ls(sl = True), "edge")
+    objName = getName(selected[0], "edge")
+    edgeNums = []
+    for edge in selected:
+        edgeNums.append(getNumber(edge, "edge"))
+    
+    allEdges = []
+    for num in edgeNums:
+        allEdges.extend(getEdges(objName, int(num)))
+    
+    cmds.select(clear = True)
+    cmds.select(allEdges)
+    #cmds.polyDelEdge( cv = True, ch = 1)
 
 def fifty():
-    first = test(1427)
-    second = test(1044)
-    cmds.select(first, second)
-    cmds.polyDelEdge( cv = True, ch = 1)
+    cmds.select(clear = True)
+    cmds.select(['pCube1.e[1427]', 'pCube1.e[1044]'])
+    start()
 
-fifty()
+
+start()
